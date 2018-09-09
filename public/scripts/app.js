f9a8e2ab51f1dd62b8865d96abecb0f112d2b80b
Daniel Kapper <dkapper01@gmail.com>
2018-09-08 22:03:36 -0400

    added the ablily to add a item and map to list the items

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,38 +5,72 @@ console.log("App is working with cat");
 var user = {
   title: "List App",
   subtitle: "An app that helps you keep track of things",
-  options: ["one", "two"]
+  options: []
 };
 
 var onFromSubmit = function onFromSubmit(e) {
   e.preventDefault();
-  c;
+  var option = e.target.elements.option.value;
+
+  if (option) {
+    user.options.push(option);
+    e.target.elements.option.value = "";
+    app();
+  }
 };
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
-    "h1",
+var removeAll = function removeAll() {
+  user.options = [];
+  app();
+};
+var appRoot = document.getElementById("app");
+
+var app = function app() {
+  var template = React.createElement(
+    "div",
     null,
-    user.title
-  ),
-  React.createElement(
-    "p",
-    null,
-    user.subtitle
-  ),
-  React.createElement(
-    "form",
-    { onSubmit: onFromSubmit },
-    React.createElement("input", { type: "text", name: "options" }),
+    React.createElement(
+      "h1",
+      null,
+      user.title
+    ),
+    React.createElement(
+      "p",
+      null,
+      user.subtitle
+    ),
+    React.createElement(
+      "p",
+      null,
+      user.options.length
+    ),
     React.createElement(
       "button",
+      { onClick: removeAll },
+      "Remove All"
+    ),
+    React.createElement(
+      "ol",
       null,
-      "Add Option"
+      user.options.map(function (option) {
+        return React.createElement(
+          "li",
+          { key: option },
+          option
+        );
+      })
+    ),
+    React.createElement(
+      "form",
+      { onSubmit: onFromSubmit },
+      React.createElement("input", { type: "text", name: "option" }),
+      React.createElement(
+        "button",
+        null,
+        "Add Option"
+      )
     )
-  )
-);
-
-var appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+  );
+  ReactDOM.render(template, appRoot);
+};
+app();
--- src/app.js
+++ src/app.js
@@ -3,26 +3,46 @@ console.log("App is working with cat");
 const user = {
   title: "List App",
   subtitle: "An app that helps you keep track of things",
-  options: ["one", "two"]
+  options: []
 };
 
-const onFromSubmit = (e) => {
-  e.preventDefault(); 
-c
-}
+const onFromSubmit = e => {
+  e.preventDefault();
+  const option = e.target.elements.option.value;
 
-const template = (
-  <div>
-    <h1>{user.title}</h1>
-    <p>{user.subtitle}</p>
+  if (option) {
+    user.options.push(option);
+    e.target.elements.option.value = "";
+    app();
+  }
+};
 
-    <form onSubmit={onFromSubmit} >
-      <input type='text' name='options' />
-      <button>Add Option</button>
-    </form>
-
-  </div>
-);
-
+const removeAll = () => {
+  user.options = [];
+  app();
+};
 const appRoot = document.getElementById("app");
-ReactDOM.render(template, appRoot);
+
+const app = () => {
+  const template = (
+    <div>
+      <h1>{user.title}</h1>
+      <p>{user.subtitle}</p>
+      <p>{user.options.length}</p>
+      <button onClick={removeAll}>Remove All</button>
+      <ol> 
+        {
+          user.options.map((option) => {
+            return <li key={option}>{option}</li>
+          })
+        }
+      </ol>
+      <form onSubmit={onFromSubmit}>
+        <input type="text" name="option" />
+        <button>Add Option</button>
+      </form>
+    </div>
+  );
+  ReactDOM.render(template, appRoot);
+};
+app();
