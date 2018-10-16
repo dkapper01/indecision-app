b7f7da42368345b88581fa2c27f69c059b9915ee
Daniel Kapper <dkapper01@gmail.com>
2018-10-16 12:22:52 -0400

    Refactor

--- src/playground/arrow-function.js
+++ src/playground/arrow-function.js
@@ -1,4 +1,7 @@
+// yarn  babel src/playground/app.js --out-file=public/scripts/app.js --presets=env,react --watch
+
 class Counter extends React.Component {
+
   constructor(props) {
     super(props);
 
